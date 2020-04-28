d9e275bad4ba98447ab1f1aa90c1caee0816a6ef
angellovc <vc.angel@outlook.com>
2020-04-28 21:05:02 +0000

    6-print_comb3.py  style was fixed

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-for tens in range (0, 10):
-    for units in range (tens + 1, 10):
+for tens in range(0, 10):
+    for units in range(tens + 1, 10):
         if units == 9 and tens == 8:
             print("{}{}".format(tens, units))
         else:
