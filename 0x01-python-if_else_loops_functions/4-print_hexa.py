8fda13445e38475b72da73dd680d9ebe75bdae9d
angellovc <vc.angel@outlook.com>
2020-04-28 18:40:35 +0000

    4-print_hexa.py was modified

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 """ print the hex numbers between 0 and 98 """
 for i in range(0, 99):
-    print("{}".format(hex(i)))  # print hex numbers
+    print("{} = {}".format(i, hex(i)))  # print hex numbers
