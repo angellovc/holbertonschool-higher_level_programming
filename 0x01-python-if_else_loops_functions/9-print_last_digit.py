e787c00963734f739a8dfbf43640f0061abd2fd1
angellovc <vc.angel@outlook.com>
2020-04-28 23:11:58 +0000

    9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,4 +2,8 @@
 
 
 def print_last_digit(number):
-    print("{:d}".format(number % 10), end='')
+    if number < 0: #
+        lastdigit = -1 * (abs(number) % 10)  # get las digit in negative number
+    else:  # last digit integer number
+        lastdigit = number % 10
+    print("{:d}".format(lastdigit), end='')
