b80adbdb4ca4a96afcf83103c9620885b144350c
angellovc <vc.angel@outlook.com>
2020-04-28 22:57:46 +0000

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" conver a string lower characters into a upper """
 
 
 def uppercase(str):
+    """ conver a string lower characters into a upper """
     for letter in str:
         if ord(letter) >= 97 and ord(letter) <= 122:
             letter = chr(ord(letter) - 32)
