8d5b35e1d7a57d9d8053db04e86954ce7a489848
angellovc <vc.angel@outlook.com>
2020-04-29 02:56:34 +0000

    13-insert_number.c was fixed

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,6 +1,23 @@
 #include "lists.h"
 #include <stdio.h>
 /**
+ *new_listint - make a new listint struct
+ *@n: element of the new struct
+ *Return: pointer to a new struct
+ */
+listint_t *new_listint(int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == '\0')
+		return ('\0');
+	new->n = 0;
+	new->next = '\0';
+	new->n = n;
+	return (new);
+}
+/**
  *insert_node - insert a node into a linked list according to digits order
  *@head: head of the linked list
  *@number: new number
@@ -8,32 +25,36 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new, *tmp;
+	listint_t *new = '\0', *node;
 
-	head[1] = head[0];
-	while (head[1] != '\0')
+	if (head == '\0')
+		return ('\0');
+	if (head[0] == '\0')
 	{
-		if (head[1]->next == '\0' || number <= head[1]->next->n)
-		{
-			new = malloc(sizeof(listint_t));
-			if (new == '\0')
-				return ('\0');
-			new->n = number;
-			if (head[1]->next == '\0' && number < head[1]->n)
-			{
-				tmp = head[1];
-				head[0] = new;
-				new->next = tmp;
-			}
-			else
-			{
-				tmp = head[1]->next;
-				head[1]->next = new;
-				new->next = tmp;
-			}
-			break;
-		}
-		head[1] = head[1]->next;
+		new = malloc(sizeof(listint_t));
+		if (new == '\0')
+			return ('\0');
+		new->n = number;
+		head[0] = new;
+		return (new);
 	}
+	node = head[0];
+	if (node != '\0' && number <= node->n)
+	{
+		new = new_listint(number);
+		if (new == '\0')
+			return ('\0');
+		new->next = node;
+		head[0] = new;
+		return ('\0');
+	}
+	else
+		while (node->next != '\0' && number > node->next->n)
+			node = node->next;
+	new = new_listint(number);
+	if (new == '\0')
+		return ('\0');
+	new->next = node->next;
+	node->next = new;
 	return (new);
 }
