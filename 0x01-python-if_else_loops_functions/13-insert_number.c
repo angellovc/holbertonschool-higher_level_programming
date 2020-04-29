6a1a2224ceb36a58e49b79dc3d9a194fd611e932
angellovc <vc.angel@outlook.com>
2020-04-29 01:01:27 +0000

    new function to manipulate linkes lists was added

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ *insert_node - insert a node into a linked list according to digits order
+ *@head: head of the linked list
+ *@number: new number
+ *Return: null if its fail, pointer to a new node if it success
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new, *tmp;
+
+	head[1] = head[0];
+	while (head[1] != '\0')
+	{
+		if (number <= head[1]->n || head[1]->next == '\0')
+		{
+			new = malloc(sizeof(listint_t));
+			if (new == '\0')
+				return ('\0');
+			new->n = number;
+			if (head[1]->next == '\0' && number < head[1]->n)
+			{
+				tmp = head[1];
+				head[0] = new;
+				new->next = tmp;
+			}
+			else
+			{
+				tmp = head[1]->next;
+				head[1]->next = new;
+				new->next = tmp;
+			}
+			break;
+		}
+		head[1] = head[1]->next;
+	}
+	return (new);
+}
