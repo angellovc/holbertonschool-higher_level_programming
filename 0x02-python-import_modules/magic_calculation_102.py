6239139b47a415c636c91f3547692dffec3b8873
angellovc <vc.angel@outlook.com>
2020-05-01 01:15:47 +0000

    magic_calculation_102.py was added

---
+++ 0x02-python-import_modules/magic_calculation_102.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+a = 1
+b = 0
+
+def sub(a, b):
+    """My subtraction function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a - b
+    """
+    return (a - b)
+
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
