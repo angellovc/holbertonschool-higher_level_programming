fa998de5eb9d5a09d3b7d82374c6f034e6ae5c02
angellovc <vc.angel@outlook.com>
2020-05-05 23:18:36 +0000

    7-add_tuple.py was added

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    """ make the sum of tuple elements
+    use zip to group all the elements in his positions
+    map to sum it and tuple to convert the return into a tuple
+    """
+    if len(tuple_a) < 1:  # concatenate elements when tuple is void
+        tuple_a += (0, 0)
+    elif len(tuple_a) < 2:
+        tuple_a += (0,)
+    if len(tuple_b) < 1:
+        tuple_b += (0, 0)
+    elif len(tuple_b) < 2:
+        tuple_b += (0,)
+    return tuple(map(sum, zip(tuple_a, tuple_b)))
