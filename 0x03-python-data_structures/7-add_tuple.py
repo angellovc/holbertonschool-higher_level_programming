003bbaf7e6ec85676dbb1bc12c7f021bdcce15f4
angellovc <vc.angel@outlook.com>
2020-05-05 23:23:08 +0000

    7-add_tuple.py was fixed

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -14,4 +14,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_b += (0, 0)
     elif len(tuple_b) < 2:
         tuple_b += (0,)
+    if len(tuple_a) > 2:
+        tuple_a = tuple_a[0:2]
     return tuple(map(sum, zip(tuple_a, tuple_b)))
