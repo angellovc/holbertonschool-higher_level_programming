17d80b6490e9764dc18be2588c2296211c6bf251
angellovc <vc.angel@outlook.com>
2020-05-05 23:24:52 +0000

    7-add_tuple.py was fixed

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,8 +3,8 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
     """ make the sum of tuple elements
-    use zip to group all the elements in his positions
-    map to sum it and tuple to convert the return into a tuple
+    use zip to group all the interative elements in the same index
+    into a tuple, map to sum it and tuple to convert the return into a tuple
     """
     if len(tuple_a) < 1:  # concatenate elements when tuple is void
         tuple_a += (0, 0)
