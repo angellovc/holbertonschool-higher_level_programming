7eacfeb602c2831f517baada0c2322a6f2ba1e5f
angellovc <vc.angel@outlook.com>
2020-05-05 18:09:37 +0000

    1-element_at.py was updated

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,5 +3,5 @@
 def element_at(my_list, idx):
     """ return the idx element of the list """
     if idx < 0 or idx >= len(my_list):
-        return -1
+        return None
     return my_list[idx]
