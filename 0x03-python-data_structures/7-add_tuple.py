84c48ec582f85699cffda7eda29d08ab546f6824
angellovc <vc.angel@outlook.com>
2020-05-05 23:39:50 +0000

    7-add_tuple.py was improved

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,14 +6,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
     use zip to group all the interative elements in the same index
     into a tuple, map to sum it and tuple to convert the return into a tuple
     """
-    if len(tuple_a) < 1:  # concatenate elements when tuple is void
-        tuple_a += (0, 0)
-    elif len(tuple_a) < 2:
-        tuple_a += (0,)
-    if len(tuple_b) < 1:
-        tuple_b += (0, 0)
-    elif len(tuple_b) < 2:
-        tuple_b += (0,)
-    if len(tuple_a) > 2:
-        tuple_a = tuple_a[0:2]
-    return tuple(map(sum, zip(tuple_a, tuple_b)))
+    tuple_a += (0, 0)  # add 0 to fill void tuples
+    tuple_b += (0, 0)
+    return tuple(map(sum, zip(tuple_a[0:2], tuple_b[0:2])))
