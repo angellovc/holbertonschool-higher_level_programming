78da148ea6c9207cd23b63d178b387ffd7b36bcf
angellovc <vc.angel@outlook.com>
2020-05-07 20:38:01 +0000

    7-update_dictionary.py was added

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+
+def update_dictionary(a_dictionary, key, value):
+    if (a_dictionary.setdefault(key, value) != value):
+        a_dictionary[key] = value
+    return a_dictionary
