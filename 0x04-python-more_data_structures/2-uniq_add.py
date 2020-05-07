5eb1678fdaf874fd91412708ac52d7563f22d997
angellovc <vc.angel@outlook.com>
2020-05-07 17:46:15 +0000

    2-uniq_add.py was fixed

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,9 +2,5 @@
 
 
 def uniq_add(my_list=[]):
-    """ sum each element of a list excluding the occurences """
-    depured_list = []
-    for number in my_list:  # delete the occurrences
-        if number not in depured_list:
-            depured_list.append(number)
-    return sum(depured_list)
+    depured_list = set(my_list)
+    return sum(list(depured_list))
