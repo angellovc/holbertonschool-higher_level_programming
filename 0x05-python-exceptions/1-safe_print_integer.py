5675d9a4b8ee19aebd9405cf52ac1b33ccbf5320
angellovc <vc.angel@outlook.com>
2020-05-18 17:14:37 +0000

    1-safe_print_integer.py was fixed

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -5,6 +5,5 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
     except ValueError:
-        print("{} is not an integer".format(value))
-        return True
+        return False
     return True
