38371f1950d802ad8c97faa3ca1dbd8491a157bd
angellovc <vc.angel@outlook.com>
2020-05-18 21:27:37 +0000

    101-safe_function.py was added

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,5 +5,5 @@ import sys
 def safe_function(fct, *args):
     try:
         return fct(args[0], args[1])
-    except (ZeroDivisionError, IndexError) as error:
+    except (ZeroDivisionError, IndexError, TypeError) as error:
         print("Exception: {}".format(error), file=sys.stderr)
