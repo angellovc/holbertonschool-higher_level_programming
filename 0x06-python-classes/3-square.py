aa7d42e267792207f30e908df07aa438ffed50b5
angellovc <vc.angel@outlook.com>
2020-05-19 21:46:50 +0000

    3-square.py was added

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Class Square module """
+
+
+class Square:
+    """Square class
+    Attributes:
+        size: represent the size of square
+    Methods:
+        area: get the area of the square class
+    """
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
