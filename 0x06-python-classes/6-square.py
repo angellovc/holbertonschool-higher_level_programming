69088e0bcc63f3c8ad53cd13a35e62102a6384b1
angellovc <vc.angel@outlook.com>
2020-05-20 00:15:50 +0000

    6-square.py was added

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+""" Class Square module """
+
+
+class Square:
+    """Square class
+    Attributes:
+        size: represent the size of square
+    Methods:
+        area: get the area of the square class
+        size: size getter and setter
+        position: position getter and setter
+        my_print: print a hashtag
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        if type(position) != tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = position
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        for i in range(0, self.__size):
+            if i != 0:
+                print("")
+            for left_space in range(0, self.__position[0]):
+                print(" ", end="")
+            print("#" * self.__size, end="")
+        print("")
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        return self.__position
+
+    @property
+    def size(self):
+        return int(self.__size)
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
