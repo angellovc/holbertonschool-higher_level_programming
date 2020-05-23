bb382cc577763c540cced2e7bda962f970df0d24
angellovc <vc.angel@outlook.com>
2020-05-23 05:15:43 +0000

    6-max_integer_test.py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
 """ unittest for max_integer function module """
 import unittest
-
-
 max_integer = __import__('6-max_integer').max_integer
 
 
 class max_integer_test(unittest.TestCase):
-    
-    def positive(self):
-        """ """
-        self.assertAlmostEqual(max_integer([3, 6, 9, 12], 133))
-        self.assertAlmostEqual(max_integer([3, 6, 9, 12], 12))
 
-    def nevative_case(self):
+    def test_positive(self):
         """ """
-        self.assertEqual(max_integer([-3, -6, -9, -12], -3))
+        self.assertAlmostEqual(max_integer([3, 6, 9, 12]), 12)
+
+    def test_nevative(self):
+        """ """
+        self.assertEqual(max_integer([-3, -6, -9, -12]), -3)
