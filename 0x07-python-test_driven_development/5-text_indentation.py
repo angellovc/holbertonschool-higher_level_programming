48209b081fdf7991545a25220d65f0296a52da3f
angellovc <vc.angel@outlook.com>
2020-05-23 04:14:18 +0000

    new files added

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
+""" text_identation module """
 
 
 def text_indentation(text):
+    """
+    text_indentation - change ".:?" for two new line
+
+    Args:
+        text: string to process. Must be a string
+
+    """
     if type(text) is not str:
         raise TypeError("text must be a string")
     new_text = text
-    new_text = new_text.replace('. ', '.\n\n')
-    new_text = new_text.replace(': ', ':\n\n')
-    new_text = new_text.replace('? ', '?\n\n')
+    new_text = new_text.replace('.', '.\n\n')
+    new_text = new_text.replace(':', ':\n\n')
+    new_text = new_text.replace('?', '?\n\n')
+    new_text = new_text.replace('\n\n ', '\n\n')
     print("{}".format(new_text), end="")
