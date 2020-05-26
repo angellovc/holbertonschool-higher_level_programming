631da69a585d9a4b3be715d946d74d08dcaeaed7
angellovc <vc.angel@outlook.com>
2020-05-26 00:22:01 +0000

    8-rectangle.py was fixed

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -88,7 +88,8 @@ class Rectangle:
             errors("rect_2_no_rectangle")
         if rect_1.area() >= rect_2.area():
             return rect_1
-        return rect_2
+        else:
+            return rect_2
 
 
 def errors(error):
