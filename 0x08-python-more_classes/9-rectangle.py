a95ae1f767985e7bc69ac8a0b0ce0b76ef44949b
angellovc <vc.angel@outlook.com>
2020-05-26 00:28:46 +0000

    9-rectangle.py was modified

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -94,8 +94,6 @@ class Rectangle:
     def square(cls, size=0):
         if type(size) is not int:
             errors("size_square_no_integer")
-        if size < 0:
-            errors("size_square_negative")
         return eval("Rectangle({}, {})".format(size, size))
 
 
