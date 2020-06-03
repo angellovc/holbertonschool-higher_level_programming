007096a3e5831ab90de1bfd535907ba93c1b7acd
angellovc <vc.angel@outlook.com>
2020-06-03 00:17:02 +0000

    10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""Square class module"""
+""" Square class module """
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Make a new Square object using the Rectangle constructor
+    """ Make a new Square object using the Rectangle constructor
     Arguments:
         Rectangle {[int]} -- [Represent the size of Square]
     """
     def __init__(self, size):
-        """ Initialize square usging rectangle constructor"""
+        """ Initialize square usging rectangle constructor """
         Rectangle.__init__(self, size, size)
