a65940e00842e5409230838066b2afa19761b4b0
angellovc <vc.angel@outlook.com>
2020-06-03 22:46:18 +0000

    was addded

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
+""" add item module """
 import sys
+
 save_json = __import__('7-save_to_json_file').save_to_json_file
 load_json = __import__('8-load_from_json_file').load_from_json_file
 loaded = []
-loaded = load_json("add_item.json")
+try:
+    loaded = load_json("add_item.json")
+except FileNotFoundError:
+    args = list(sys.argv)[1:]
+    save_json(str(args), "add_item.json")
 args = list(sys.argv)[1:]
-save_json(str(args), "add_item.json")
 loaded += args
 save_json(loaded, "add_item.json")
