2250fc9d02876d3ba70b3fa435e9a0401d4f6ca4
angellovc <vc.angel@outlook.com>
2020-06-04 00:08:55 +0000

    12-student.py was added

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" class Student module """
+
+
+class Student:
+    """Student class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """json obj"""
+        filtered = {}
+        dic = self.__dict__
+        for key in dic:
+            if attrs is not None and key in attrs:
+                filtered[key] = dic[key]
+        return filtered
