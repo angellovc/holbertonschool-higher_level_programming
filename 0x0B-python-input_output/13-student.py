fdddc1a25d45f509ef0df688edafa74fbf0fe763
angellovc <vc.angel@outlook.com>
2020-06-04 01:05:48 +0000

    13-student.py

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" class Student module """
+
+
+class Student:
+    """Student class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """json obj"""
+        filtered = {}
+        dic = self.__dict__
+        for key in dic:
+            if attrs is None or key in attrs:
+                filtered[key] = dic[key]
+        return filtered
+
+    def reload_from_json(self, json):
+        """ reload data from json """
+        for key in json:
+            self.key = json[key]
