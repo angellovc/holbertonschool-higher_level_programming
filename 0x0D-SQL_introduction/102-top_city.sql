d965688ec59591ac471e74893b479ef5e718d4b3
angellovc <vc.angel@outlook.com>
2020-07-14 03:37:34 +0000

    103-max_state.sql

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,4 +1,5 @@
 -- displays the top 3 of cities temperature during July and August
+-- database used was https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/272/temperatures.sql
 SELECT city, AVG(value) AS avg_temp
 FROM temperatures
 WHERE month
