e0a77b36b1cb80ae75437e37838ae0de1e651125
angellovc <vc.angel@outlook.com>
2020-08-08 17:32:59 +0000

    model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,7 +2,9 @@
 """ state class module """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+
 Base = declarative_base()
+
 
 class State(Base):
     """ MySQL states table representation
@@ -11,6 +13,3 @@ class State(Base):
 
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
