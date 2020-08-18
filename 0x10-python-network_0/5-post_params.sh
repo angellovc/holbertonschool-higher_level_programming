8cd840f9b6bc151e4932c64da5e1b23872705b5c
angellovc <vc.angel@outlook.com>
2020-08-18 21:52:39 +0000

    5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #POST request to the passed URL, and displays the body of the response
-curl -sX "POST" -H "email: hr@holbertonschool.com" -H "subject: I will always be here for PLD" "$1"
+curl -X "POST" -H "email: hr@holbertonschool.com" -H "subject: I will always be here for PLD" "$1"
