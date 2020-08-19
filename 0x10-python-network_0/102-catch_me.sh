db1c0eba05dbd0c2bc2aa251d61741e240cda507
angellovc <vc.angel@outlook.com>
2020-08-19 01:13:13 +0000

    102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #chatch it
-curl -si -X "PUT" -d "user_id=98" -H "origin: HolbertonSchool" -L 0.0.0.0:5000/catch_me
+curl -s -X "PUT" -d "user_id=98" -H "origin: HolbertonSchool" -L 0.0.0.0:5000/catch_me
