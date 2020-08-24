5975ceea0212ae3fa8b1c944a1324977c8db53d8
angellovc <vc.angel@outlook.com>
2020-08-24 23:37:11 +0000

    5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -3,9 +3,9 @@
 of the variable X-Request-Id in the response header
 """
 import requests
-
+from sys import argv
 
 if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
+    url = argv[1]
     response = requests.get(url)
     print(response.headers['X-Request-Id'])
