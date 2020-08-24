a62d7f598db1b5ea792cd13c82f69cdc15ff58f6
angellovc <vc.angel@outlook.com>
2020-08-24 21:18:43 +0000

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from urllib import request
+from urllib import parse
+
+data = {}
+url = "https://intranet.hbtn.io/status"
+
+with request.urlopen(url) as response:
+    http = response.read()
+    print("Body response:")
+    print("    - type: {}".format(type(http)))
+    print("    - content: {}".format(http))
+    print("    - utf8 content: {}".format(http.decode()))
