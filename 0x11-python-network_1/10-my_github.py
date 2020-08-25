bc5d40ff21d8104c3714574f5066f76c827034d4
angellovc <vc.angel@outlook.com>
2020-08-25 02:54:35 +0000

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,8 +2,10 @@
 """ takes your Github credentials (username and password)
 and uses the Github API to display your id """
 import requests
-import sys
-token = sys.argv[2]
-user = sys.argv[1]
-response = requests.get('https://api.github.com/user', auth=(user, token))
-print(response.json().get('id'))
+from sys import argv
+
+if __name__ == "__main__":
+    token = argv[2]
+    user = argv[1]
+    response = requests.get('https://api.github.com/user', auth=(user, token))
+    print(response.json().get('id'))
