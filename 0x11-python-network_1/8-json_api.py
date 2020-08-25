4961758839fb39a1d9f81d350ac8c85b7c243895
angellovc <vc.angel@outlook.com>
2020-08-25 01:56:06 +0000

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,25 +1,43 @@
 #!/usr/bin/python3
 """ sends a POST request to http://0.0.0.0:5000/search_user
-with the letter as a parameter
-"""
+with the letter as a parameter """
 import requests
 import json
 from sys import argv
 
+
+def load_json(json):
+    data = {}
+    if json.find("{") is -1 and json.find("}") is -1:
+        raise TypeError('Not a valid JSON')
+    if len(json) < 4:
+        raise ValueError('No result')
+    json = json.replace("{", "")
+    json = json.replace("}", "")
+    json = json.replace('\n', "")
+    json = json.replace("\"", "")
+    json = json.split(",")
+    for item in json:
+        item = item.split(':')
+        if item[0] == 'id':
+            data[item[0]] = int(item[1])
+        else:
+            data[item[0]] = item[1]
+    return data
+
 if __name__ == "__main__":
-    url = "http://0.0.0.0:5000/search_user"
-    if argv[1]:
+    if len(argv) > 1 and argv[1]:
         data = {'q': argv[1]}
     else:
         data = {'q': ''}
-    response = requests.post(url, data=data)
+    url = "http://0.0.0.0:5000/search_user"
+    response = requests.post(url, data)
     try:
-        body = json.loads(response.text)
-        id = body.get('id')
-        name = body.get('name')
-        if id is None:
-            print("No result")
-        else:
-            print("[{}] {}".format(id, name))
-    except:
-        print("Not a valid JSON")
+        values = load_json(response.text)
+        id = values.get('id')
+        name = values.get('name')
+        print("[{}] {}".format(id, name))
+    except TypeError as Error:
+        print(Error)
+    except ValueError as Error:
+        print(Error)
