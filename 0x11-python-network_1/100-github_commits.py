1e29fd3e6de92206a5f75f467bbfa2139307b0b6
angellovc <vc.angel@outlook.com>
2020-08-25 03:26:47 +0000

    100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" list 10 commits (from the most recent to oldest) of the repository """
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    repo = argv[1]
+    owner = argv[2]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(
+        owner,
+        repo
+    )
+    response = requests.get(url)
+    commits = response.json()[0:11]
+    for i in range(0, 10):
+        commit_id = response.json()[i].get('sha')
+        name = response.json()[i].get('commit').get('author').get('name')
+        print ("{} {}".format(commit_id, name))
