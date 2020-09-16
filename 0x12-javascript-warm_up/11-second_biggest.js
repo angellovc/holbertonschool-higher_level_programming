6d94ed42318b984851a852ad5fb6a7daffed013d
angellovc <vc.angel@outlook.com>
2020-09-16 02:55:16 +0000

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,9 @@ let i;
 let max = 0;
 let secondMax = 0;
 for (i = 2; i < process.argv.length; i++) {
-  if (max < process.argv[i]) { secondMax = max; max = parseInt(process.argv[i]); }
+  if (max < process.argv[i] || (process.argv[i] < 0 && max <= 0)) {
+    secondMax = max;
+    max = parseInt(process.argv[i]);
+  }
 }
 console.log(secondMax);
