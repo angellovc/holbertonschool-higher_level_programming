7221305fa8af6be93fb3153a4478eaa72561c193
angellovc <vc.angel@outlook.com>
2020-09-16 03:51:56 +0000

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,13 @@
 #!/usr/bin/node
 let array;
+let i;
 if (process.argv.length < 4) {
   console.log(0);
 } else {
   array = process.argv.slice(2, process.argv.length);
   array.sort();
   if (array[0] > 0) { array.reverse(); }
-  console.log(array[1]);
+  i = 1;
+  while (array[i] === array[i - 1]) { i++; }
+  console.log(array[i]);
 }
