8be0b96417f15e839410b585e5095a2cb5e02a4e
angellovc <vc.angel@outlook.com>
2020-09-16 04:33:44 +0000

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,31 +1,5 @@
 #!/usr/bin/node
-let negative = [];
-let positive = [];
-let array = [];
-let i;
-let nIndex = 0;
-let pIndex = 0;
-if (process.argv.length < 4) {
-  console.log(0);
-} else {
-  for (i = 2; i < process.argv.length; i++) {
-    if (array[i] < 0) {
-      negative[nIndex] = parseInt(process.argv[i]);
-      nIndex++;
-    } else {
-      positive[pIndex] = parseInt(process.argv[i]);
-      pIndex++;
-    }
-  }
-  if (positive.length > 0) { negative.sort(); }
-  positive.sort();
-  negative = negative.join(' ');
-  positive = positive.join(' ');
-  array = negative + positive;
-  array = array.split(' ');
-  if (array[0] < 0 && array[array.length - 1] > 0) {
-    console.log(array[array.length - 2]);
-  } else {
-    console.log(array[1]);
-  }
-}
+let i = 0;
+const array = process.argv.slice(2, process.argv.length);
+if (array.length > 1) { array.sort(function (a, b) { return b - a; }); i = array[1]; }
+console.log(i);
