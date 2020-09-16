b123f8f77cd3376f2522f7806fd43a9d63acbf1c
angellovc <vc.angel@outlook.com>
2020-09-16 01:46:10 +0000

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 if (process.argv[2]) {
   for (let outer = 0; outer < process.argv[2]; outer++) {
+    let square = '';
     for (let inner = 0; inner < process.argv[2]; inner++) {
-      process.stdout.write('X');
+      square += 'X';
     }
-    process.stdout.write('\n');
+    console.log(square);
   }
 }
