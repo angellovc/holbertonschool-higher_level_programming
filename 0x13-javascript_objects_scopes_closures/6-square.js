681fb19e73b65d78a9aabcef3113f84fce2dd326
angellovc <vc.angel@outlook.com>
2020-09-21 17:26:24 +0000

    6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,16 +2,20 @@
 const square = require('./5-square.js');
 
 module.exports = class Square extends square {
-  charPrint (c = 'x') {
-    let row;
-    let column;
-    let square;
-    for (row = 0; row < this.height; row++) {
-      square = '';
-      for (column = 0; column < this.width; column++) {
-        square += c;
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      let row;
+      let column;
+      let square;
+      for (row = 0; row < this.height; row++) {
+        square = '';
+        for (column = 0; column < this.width; column++) {
+          square += c;
+        }
+        console.log(square);
       }
-      console.log(square);
     }
   }
 };
