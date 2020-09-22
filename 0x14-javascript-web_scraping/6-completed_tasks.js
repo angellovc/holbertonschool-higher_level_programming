7adb3dc45daef8a30ebd502b85bf26b026e8dcb9
angellovc <vc.angel@outlook.com>
2020-09-22 18:51:31 +0000

    6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,16 +2,20 @@
 
 const request = require('request');
 const url = process.argv[2];
-request(url, function (_, response) {
-  const tasks = JSON.parse(response.body);
-  const completedTasks = {};
-  let counter = 0;
-  for (let i = 0; i < tasks.length; i++) {
-    if (!completedTasks[tasks[i].userId]) { counter = 0; }
-    if (tasks[i].completed === true) {
-      counter++;
-      completedTasks[tasks[i].userId] = counter;
+request(url, function (error, response) {
+  if (error) {
+    console.log(error);
+  } else {
+    const tasks = JSON.parse(response.body);
+    const completedTasks = {};
+    let counter = 0;
+    for (let i = 0; i < tasks.length; i++) {
+      if (!completedTasks[tasks[i].userId]) { counter = 0; }
+      if (tasks[i].completed === true) {
+        counter++;
+        completedTasks[tasks[i].userId] = counter;
+      }
     }
+    console.log(completedTasks);
   }
-  console.log(completedTasks);
 });
