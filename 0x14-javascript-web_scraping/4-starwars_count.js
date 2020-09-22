498f259c7f7acfd0189dcb5a751fc0c5ea8b0f92
angellovc <vc.angel@outlook.com>
2020-09-22 17:27:23 +0000

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,11 +3,13 @@
 const request = require('request');
 const url = process.argv[2];
 const wedge = 'https://swapi-api.hbtn.io/api/people/18/';
-request(url, { headers: { 'Content-Type': 'application/json; charset=utf-8' } }, function (_, response) {
-  const films = JSON.parse(response.body).results;
-  let counter = 0;
-  for (let i = 0; i < films.length; i++) {
-    films[i].characters.map((value) => value === wedge ? counter++ : null);
+request(url, { headers: { 'Content-Type': 'application/json; charset=utf-8' } }, function (err, response) {
+  if (!err) {
+    const films = JSON.parse(response.body).results;
+    let counter = 0;
+    for (let i = 0; i < films.length; i++) {
+      films[i].characters.map((value) => value === wedge ? counter++ : null);
+    }
+    console.log(counter);
   }
-  console.log(counter);
 });
